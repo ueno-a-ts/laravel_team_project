6f1341464bfeb7e3dfb1e5cd177a8793648967dd
千田　裕耕 <>
2021-06-21 12:50:34 +0900

    cartsマイグレーションファイルの追加

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,14 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'ItemController@index');
 
 Route::get('/items/{item}', 'ItemController@show');
 
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
