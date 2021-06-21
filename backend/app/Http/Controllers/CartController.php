30b0d639da0c9325f887359ce66a2611aa404bff
千田　裕耕 <>
2021-06-21 14:54:02 +0900

    mycartsページでidが表示されるようにする

--- backend/app/Http/Controllers/CartController.php
+++ backend/app/Http/Controllers/CartController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Cart;
 
 class CartController extends Controller
 {
-    //
+    public function myCart()
+    {
+        $my_carts = Cart::all();
+        return view('carts.mycarts',compact('my_carts'));
+        
+    }
 }
---
+++ backend/resources/views/carts/mycarts.blade.php
@@ -0,0 +1,15 @@
+
+<div class="container-fluid">
+ <div class="">
+     <div class="mx-auto" style="max-width:1200px">
+         <h1 class="text-center font-weight-bold" style="color:#555555;
+             font-size:1.2em; padding:24px 0px;">カートの中身</h1>
+         <div class="card-body">
+            @foreach($my_carts as $my_cart)
+                    {{$my_cart->item_id}}<br>
+                    {{$my_cart->user_id}}<br>
+            @endforeach
+         </div>
+     </div>
+ </div>
+</div>
